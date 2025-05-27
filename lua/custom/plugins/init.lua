646f2ef40a5dd2eb31702df11ac7f35bd26271eb
natejank <njj3397@rit.edu>
2025-05-26 23:06:59 -0400

    add plugin to make marks update to the last position in a file

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,21 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+--
+
+local function cwd_name()
+  return vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
+end
+
+return {
+  {
+    'BartSte/nvim-project-marks',
+    lazy = false,
+    config = function()
+      require('projectmarks').setup {
+        shadafile = '~/.cache/shadas/' .. cwd_name() .. '.shada',
+        message = '',
+      }
+    end,
+  },
+}
